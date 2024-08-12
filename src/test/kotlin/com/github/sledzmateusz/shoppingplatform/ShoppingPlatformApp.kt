40a95d3dec59fa0186d43bfe82ba85ac8e399edc
sledzmateusz <mateusz.sledz@outlook.com>
2024-08-12 20:11:20 +0200

    Initial commit

---
+++ src/test/kotlin/com/github/sledzmateusz/shoppingplatform/ShoppingPlatformApp.kt
@@ -0,0 +1,12 @@
+package com.github.sledzmateusz.shoppingplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingPlatformApp {
+
+  @Test
+  fun contextLoads() {
+  }
+}
